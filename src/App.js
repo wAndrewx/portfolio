f71ed515afcecff712a71888b666a9ebead92c7a
Andrew Huynh <andrew.huynh17@gmail.com>
2021-05-02 21:19:05 -0400

    custom theme and fonts init

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from 'react';
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
+  Flex,
 } from '@chakra-ui/react';
+import { andrewTheme } from './theme/andrewTheme'
+
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={andrewTheme}>
+      <Flex >
+        
+
+      </Flex>
     </ChakraProvider>
   );
 }
