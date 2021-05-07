547a40f105b00dfeab8eddca5b36b16aa727f67c
Andrew Huynh <andrew.huynh17@gmail.com>
2021-05-06 21:35:58 -0400

    logo now functions as about me

---
+++ src/components/main-src/logo/LogoName.js
@@ -0,0 +1,25 @@
+import { Text } from '@chakra-ui/layout';
+import { Tooltip } from '@chakra-ui/tooltip';
+
+export const LogoName = ({colorStroke,backgroundColor}) => {
+  return (
+    <Tooltip
+      label="ABOUT ME"
+      bg={colorStroke}
+      color={backgroundColor}
+      placement="top-start"
+      rounded="4"
+      hasArrow
+      arrowShadowColor={backgroundColor}
+    >
+      <Text
+        fontSize="4em"
+        fontWeight="bold"
+        color="highlight"
+        lineHeight={['55px']}
+      >
+        ANDREW HUYNH
+      </Text>
+    </Tooltip>
+  );
+};
